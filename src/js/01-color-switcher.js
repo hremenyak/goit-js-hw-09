f9d70fa30d8d154d88e448396ca8b463c4b9d606
hremenyak <hremenyak@gmail.com>
2022-09-09 20:02:31 +0200

    complete task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = 0;
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', '');
+  refs.stopBtn.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  refs.stopBtn.setAttribute('disabled', '');
+  refs.startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
